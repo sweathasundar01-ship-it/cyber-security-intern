f4b7b1cd7a26a8712114b6664b545acbc03e6be8
sweathasundar01-ship-it <sweathasundar01@gmail.com>
2026-05-23 12:32:20 +0530

    Add files via upload

---
+++ sweathaint4.py
@@ -0,0 +1,66 @@
+'''a=int(input("tamil mark:"))
+b=int(input("English mark"))
+c=int(input("Maths mark:"))
+d=int(input("science mark:"))
+e=int(input("Social Mark:"))
+if (a,b,c,d,e > 35):
+    print("you are passed")
+else:
+    print("sorry you are fail")
+total= a+b+c+d+e
+print("Total Mark = ",total)
+avg=total/5
+print("Average value = ",avg)
+
+if avg > 95:
+    print("You are eligible for Medical seat!")
+elif avg>80:
+    print("You are eligible for Engineering!")
+elif avg > 70:
+    print("You are eligible for Science! ")
+elif avg>60:
+    print("You are eligible for Arts!!")
+else:
+    print("You are not eligible!!")
+
+a = int (input("Enter your value:"))
+b = int (input("Enter your value:"))
+c= int (input("Enter tyour value:"))
+if a>b:
+    print("A is greater value")
+elif a>c:
+    print("A is greater value ")
+elif b>c:
+    print("B is greater value")
+else:
+    print("C is greater value")'''
+a= int(input("tamil mark = "))
+b= int(input("English mark = "))
+c=int(input("Maths mark ="))
+d=int (input("chemistry = "))
+e=int(input("Computer science = "))
+f=int(input("pys mark = "))
+total= a+b+c+d+e+f
+print("Total Marks = ",total)
+avg=total/6
+print("Average = ",avg)
+if avg > 90:
+    print("You are eligib le for medical!!")
+elif avg >80:
+    print("You are eligible for Engnieering!!")
+elif avg > 70:
+    print("You are eligible for Science!!")
+else:
+    print("You are not eligible!")
+
+
+
+
+
+
+
+
+
+
+
+
