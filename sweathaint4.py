0dfe7d63a94bfe66236e226b7ed79012b2cc9e36
sweathasundar01-ship-it <sweathasundar01@gmail.com>
2026-05-25 13:10:21 +0530

    Add files via upload

--- sweathaint4.py
+++ sweathaint4.py
@@ -1,66 +1,64 @@
-'''a=int(input("tamil mark:"))
-b=int(input("English mark"))
-c=int(input("Maths mark:"))
-d=int(input("science mark:"))
-e=int(input("Social Mark:"))
-if (a,b,c,d,e > 35):
-    print("you are passed")
+'''a= int(input("Enter the year:"))
+if (a % 4 == 0 ):
+    print(a,"it is leap year")
 else:
-    print("sorry you are fail")
-total= a+b+c+d+e
-print("Total Mark = ",total)
-avg=total/5
-print("Average value = ",avg)
+    print(a,"it not leap year")
 
-if avg > 95:
-    print("You are eligible for Medical seat!")
-elif avg>80:
-    print("You are eligible for Engineering!")
-elif avg > 70:
-    print("You are eligible for Science! ")
-elif avg>60:
-    print("You are eligible for Arts!!")
+a=int(input("Enter the value:"))
+if a % 2 !=0:
+    print(a,"is even")
 else:
-    print("You are not eligible!!")
+    print(a,"is odd")
 
-a = int (input("Enter your value:"))
-b = int (input("Enter your value:"))
-c= int (input("Enter tyour value:"))
-if a>b:
-    print("A is greater value")
-elif a>c:
-    print("A is greater value ")
-elif b>c:
-    print("B is greater value")
+
+
+age=int(input("Age:"))
+if age < 5:
+    print("free for kids")
+elif age >= 5 and age <  15:
+    print("your entry amount is 300")
+elif age >= 15  and age < 35:
+    print("Your entry amount is 400")
+elif age >=35 and age < 60:
+    print("Your entry amount is 500")
+elif age >=60 and age < 80:
+    print("Your entry amount is 600")
 else:
-    print("C is greater value")'''
-a= int(input("tamil mark = "))
-b= int(input("English mark = "))
-c=int(input("Maths mark ="))
-d=int (input("chemistry = "))
-e=int(input("Computer science = "))
-f=int(input("pys mark = "))
-total= a+b+c+d+e+f
-print("Total Marks = ",total)
-avg=total/6
-print("Average = ",avg)
-if avg > 90:
-    print("You are eligib le for medical!!")
-elif avg >80:
-    print("You are eligible for Engnieering!!")
-elif avg > 70:
-    print("You are eligible for Science!!")
+    print("You are not allowed for the themepark")
+
+age=int(input("Enter the age:"))
+test=input("you are already take the test?(yes/no):")
+if age <18:
+    print("you are under 18")
+elif age >= 18 and test =="yes":
+    print("you are eligible for license")
+elif age >=18 and test=="no":
+    print("You are not eligible for lincense")
+
+mark=int(input("Enter the mark:"))
+attendance=int(input("Enter the attendance percentage:"))
+if mark >=50 and attendance >= 75:
+    print("You are passed with good attendance!")
+elif mark >= 50:
+    print("You are passed but attendaance is low!")
+elif mark <=50:
+    print("you are fail but attendance is good")
 else:
-    print("You are not eligible!")
+    print("both is low")
+'''
 
-
-
-
-
-
-
-
-
-
-
+a=int(input("Enter the mark1:"))
+b=int(input("Enter the mark2:"))
+c=int(input("enter the mark3:"))
+print("person1",a)
+print("person2",b)
+print("person3",c)
+total=a+b+c
+print("total=",total)
+if a >= b and a >= c:
+    print("Person1 is greater")
+elif b >= a and b >= c:
+    print("Person2 is greater")
+else:
+    print("Person3 is greater")
 
