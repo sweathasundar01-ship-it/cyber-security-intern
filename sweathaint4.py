cf04beed0e6bfe925192ad69c1f9888be1e553ca
sweathasundar01-ship-it <sweathasundar01@gmail.com>
2026-05-28 11:40:23 +0530

    Add files via upload

--- sweathaint4.py
+++ sweathaint4.py
@@ -1,31 +1,43 @@
-student1=input("Enter the student name:")
-s1_subject1=int(input("Enter student1 mark:"))
-s1_subject2=int(input("Enter student1 mark:"))
-s1_subject3=int(input("Enter stident1 mark:"))
-total_s1=s1_subject1+ s1_subject2+s1_subject3
-avg1=total_s1/3
-print("total = ",total_s1)
-print("avg = ",avg1)
-student2=input("Enter the student name:")
-s2_subject1=int(input("Enter student2 mark:"))
-s2_subject2=int(input("Enter student2 mark:"))
-s2_subject3=int(input("Enter stident2 mark:"))
-total_s2=s2_subject1+ s2_subject2+s2_subject3
-avg2=total_s2/3
-print("total = ",total_s2)
-print("avg = ",avg2)
-student3=input("Enter the student name:")
-s3_subject1=int(input("Enter student2 mark:"))
-s3_subject2=int(input("Enter student2 mark:"))
-s3_subject3=int(input("Enter stident2 mark:"))
-total_s3=s3_subject1+ s3_subject2+s3_subject3
-avg3=total_s3/3
-print("total = ",total_s3)
-print("avg = ",avg3)
-if total_s1 > total_s2 and total_s1 > total_s3:
-    print(student1,"got the greater mark")
-elif total_s2 > total_s3 and total_s2 > total_s1:
-    print(student2,"got the greater mark")
+food = input("Enter the food:")
+quantity=int(input("Quantity of the food:"))
+if food == "pizza":
+    print("pizza is available\n")
+    price= 250
+    print("pizza price is",price)
+    print("quantity of the pizza is \n",quantity)
+    delivary=input("is delivary is available (yes or no):")
+    if delivary == "yes":
+        print("The delivary is available\n")
+    else:
+        print("The delivary is unavailable")
+    print("------------------")
+    print("Total amount: ",price*quantity)
+    print("------------------")
+elif food == "burgar":
+    print("Burgar is available\n")
+    price=300
+    print("Burgar price is",price)
+    print("quantity of the Burgar is ",quantity)
+    delivary=input("is delivary is available (yes or no):")
+    if delivary == "yes":
+        print("The delivary is available\n")
+    else:
+        print("The delivary is unavailable\n")
+    print("------------------")
+    print("Total amount: ",price*quantity)
+    print("------------------")
+elif food == "shawarma":
+    print("Shawarma is available\n")
+    price= 150
+    print("Shawarma price is",price)
+    print("quantity of the Shawarma is ",quantity)
+    delivary=input("is delivary is available (yes or no):")
+    if delivary == "yes":
+        print("The delivary is available\n")
+    else:
+        print("The delivary is unavailable\n") 
+    print("------------------")
+    print("Total amount: ",price*quantity)
+    print("------------------")
 else:
-    print(student3,"got the greater mark")
-    
+    print("Sorry the food is unavailable!!!")
